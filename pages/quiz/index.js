6efbf19ff48ddf0c0df649cf5009081b436357dc
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2025-11-16 13:58:11 +0530

    increased the toughness

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -14,6 +14,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import QuizPlayer from "../../components/QuizPlayer";
 import Navbar from "../../components/Navbar";
 import { toast } from "sonner";
+import { getMarvelName } from "../../utils/getMarvelName";
 
 export default function QuizArena() {
   const [user, setUser] = useState(null);
